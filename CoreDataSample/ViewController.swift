7d67a33621ca555258adf8a6c793052c3371f8e7
yuu <yuuxzy@gmail.com>
2019-09-19 11:24:29 +0900

    Initial Commit

---
+++ CoreDataSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreDataSample
+//
+//  Created by 14-0254 on 2019/09/19.
+//  Copyright © 2019 Yusuke Binsaki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
